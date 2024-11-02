8ee2640fff22c8cde695015502a141145b6781b6
Carson Mensink <mensinkcarson@gmail.com>
2024-11-01 22:00:23 -0500

    removed alaska and hawaii\ncurrently looping through all other states\nneed to put data into json file

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 from config import API_KEY
 
 def fetch_api_data(url, params):
@@ -9,20 +10,38 @@ def fetch_api_data(url, params):
         return response.json()
     else:
         print("FAILED")
+        print(f'\n\n\n\n{params}\n\n\n\n\n')
         response.raise_for_status()
 
-params = {
-    'commodity_desc': 'CORN',
-    'year__GE': '2024',
-    'state_alpha': 'VA',
-    'format': 'json'
-}
 
+def loop_through_states():
+    state_abbreviations = [
+        'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+        'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+        'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+        'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+        'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+    ]
+    for state in state_abbreviations:
+        pull_data(state)
+        
 
-url = "https://quickstats.nass.usda.gov/api/api_GET/"
-data = fetch_api_data(url, params)
+def pull_data(state):
+    params = {
+        'commodity_desc': 'CORN',
+        'year__GE': '2024',
+        'state_alpha': state,
+        'format': 'json'
+    }
+    url = "https://quickstats.nass.usda.gov/api/api_GET/"
+    data = fetch_api_data(url, params)
+    add_data_to_file(data)
 
-with open('crop_data.json', 'w') as json_file:
-    json.dump(data, json_file, indent=4)
+def add_data_to_file(data):
+    pass
 
-print("Data fetched and saved to crop_data.json")
+def main():
+    loop_through_states()
+
+main()
+
