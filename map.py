c02d89d0d9568d5d4ff0d0624dd3fb72a482a4ad
Seth Keenan <seth.j.keenan@gmail.com>
2024-11-02 11:23:22 -0500

    Non scrolling

--- map.py
+++ map.py
@@ -2,10 +2,34 @@ import folium
 from folium.plugins import MiniMap
 import tkinter as tk
 from tkinter import ttk
+import requests
 
 class Map:
     def __init__(self):
-        self.m = folium.Map(location=(39.30, -98.5795), zoom_start=5)
+        self.m = folium.Map(
+            location=(39.30, -98.5795), 
+            zoom_start=5,
+            dragging = False,
+            zoom_control = False,
+            scrollWheelZoom = False
+            )
+        
+        state_geo = requests.get(
+        "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
+        ).json()
+        
+        folium.Choropleth(
+            geo_data=state_geo,
+            name="choropleth",
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="YlGn",
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            legend_name="Unemployment Rate (%)",
+
+        )
+
         self.mini_map = MiniMap()
         self.m.add_child(self.mini_map)
         self.m.save('map.html')
