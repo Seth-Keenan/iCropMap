c771997b815c5ad8aaa1c76b7dc7f075598a7021
Carson Mensink <mensinkcarson@gmail.com>
2024-11-02 00:10:54 -0500

    sending 1 request and added main to add crop parameter

--- data.py
+++ data.py
@@ -11,18 +11,6 @@ def fetch_api_data(url, params):
     else:
         print("FAILED")
         response.raise_for_status()
-
-
-def loop_through_states():
-    state_abbreviations = [
-        'AL', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-        'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-        'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-        'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-        'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
-    ]
-    for state in state_abbreviations:
-        pull_data(state)
         
 
 def filter_data(data):
@@ -31,14 +19,17 @@ def filter_data(data):
         state_data = json.load(f)
     dict_list = state_data['data']
     for item in dict_list:
-        if item.get("short_desc") == "CORN - ACRES PLANTED":
-            filtered_data.append(item)
+        if item.get("short_desc") == "CORN - ACRES PLANTED" and item.get("reference_period_desc") == "YEAR":
+            new_item = {"crop": item.get("commodity_desc")}
+            new_item["state"] = item.get("state_name")
+            new_item["amount"] = item.get("Value")
+            filtered_data.append(new_item)
     return filtered_data
-def pull_data(state):
+
+def pull_data(crop, data_list):
     params = {
-        'commodity_desc': 'CORN',
+        'commodity_desc': crop,
         'year__GE': '2024',
-        'state_alpha': state,
         'format': 'json'
     }
     url = "https://quickstats.nass.usda.gov/api/api_GET/"
@@ -52,7 +43,10 @@ def add_data_to_file(data, filepath):
     with open(filepath, 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
-if __name__ == "__main__":
+def main(crop):
     data_list = []
-    pull_data('IA')
+    pull_data(crop, data_list)
     add_data_to_file(data_list, 'crop_data.json')
+    
+if __name__ == "__main__":
+    main()
