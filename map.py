f70e4d159fbbc3e68a3baff270bf2c6fd195b61d
Seth Keenan <seth.j.keenan@gmail.com>
2024-11-02 12:14:00 -0500

    Added outline for map

--- map.py
+++ map.py
@@ -3,6 +3,7 @@ from folium.plugins import MiniMap
 import tkinter as tk
 from tkinter import ttk
 import requests
+import pandas
 
 class Map:
     def __init__(self):
@@ -11,28 +12,26 @@ class Map:
             zoom_start=5,
             dragging = False,
             zoom_control = False,
-            scrollWheelZoom = False
+            scrollWheelZoom = False,
+            doubleClickZoom = False
             )
         
         state_geo = requests.get(
         "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
         ).json()
+
+        state_data = pandas.read_csv("crop_data.csv")
         
         folium.Choropleth(
             geo_data=state_geo,
             name="choropleth",
-            columns=["State", "Unemployment"],
+            data=state_data,
+            columns=["State", "Amount"],
             key_on="feature.id",
             fill_color="YlGn",
             fill_opacity=0.7,
-            line_opacity=0.2,
-            legend_name="Unemployment Rate (%)",
-
-        )
-
-        self.mini_map = MiniMap()
-        self.m.add_child(self.mini_map)
-        self.m.save('map.html')
-
+            line_opacity=0.5,
+            legend_name="Crop",
+        ).add_to(self.m)
 
     
