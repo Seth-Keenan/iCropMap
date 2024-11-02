41b13999e25b49e28182b0ae718d4c273173cb14
Seth Ek <sge3141@gmail.com>
2024-11-01 23:05:31 -0500

    Updated About and Contact Pages

--- cropmap/src/pages/About.js
+++ cropmap/src/pages/About.js
@@ -2,5 +2,10 @@ import React from 'react';
 
 export default function About()
 {
-    return <p>about page</p>
+    return (
+        <div>
+            <h2>Tiger-Hacks 2024</h2>
+            <p>iCROP will let you see crops grown throughout the U.S.</p>
+        </div>
+    )
 }
--- cropmap/src/pages/Contact.js
+++ cropmap/src/pages/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-export default function About()
-{
-    return <p>Contact us</p>
+export default function About() {
+    return (
+        <div>
+            <p>Contact Information: <a href="mailto:sgebm6@mail.missouri.edu">Seth Ek at sgebm6@mail.missouri.edu</a></p>
+        </div>
+    );
 }
